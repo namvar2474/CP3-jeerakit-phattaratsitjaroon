1411fdd7beb8126214b998a8fd346bb8656b813e
namvar2474 <84053529+namvar2474@users.noreply.github.com>
2021-05-17 08:23:34 +0700

    Add files via upload

---
+++ Exercise4_jeerakit_v.py
@@ -0,0 +1,10 @@
+FE = float(input("put your Foundation English score"))
+GB = float(input("put your General Business score"))
+ICS = float(input("put your Introduction to Computer Systems score"))
+C_program =  float(input("put your Computer Programming score"))
+
+print ("--- Your Score ---")
+print ("Foundation English" , FE)
+print ("General Business" , GB )
+print ("Introduction to Computer Systems" , ICS)
+print ("Computer Programming" , C_program)
